8f4d9e8565e7fe2d7072061ba4bdb89f0c12c3b6
yousseftawfeek <yousseftawfeek1000@gmail.com>
2024-01-08 02:15:55 +0000

    done

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,21 @@
+4-print_square.py
+#!/usr/bin/python3
+"""Defines a square-printing function."""
+
+
+def print_square(size):
+    """
+    Print a square with the # character
+    """
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    for i in range(size):
+        [print("#", end="") for j in range(size)]
+        print("")
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/4-print_square.txt")
