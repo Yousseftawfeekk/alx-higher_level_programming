b91838433bd91bbbc18232b08671f35073b8be12
yousseftawfeek <yousseftawfeek1000@gmail.com>
2024-01-09 00:35:53 +0000

    done

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+''' module to sort mylist'''
+
+class MyList(list):
+    ''' my list class '''
+
+    def print_sorted(self):
+	''' method for printing sorted list'''
+        print(sorted(self))
+
+	
